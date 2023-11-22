b3ee64ae6971a4c7ff9923cb5bf06415adb240da
Alin Grigorescu <alin.grig87@gmail.com>
2023-11-22 21:35:46 +0200

    ProductCard component | Home Page | use state | use effect

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import ProductCard from '../components/Products/ProductCard';
 
 const Home = () => {
-	return <div>Home</div>;
+	const [products, setProducts] = useState(null);
+
+	useEffect(() => {
+		const fetchProducts = async () => {
+			const response = await fetch(
+				'https://654bc6c15b38a59f28efb3a2.mockapi.io/products'
+			);
+			const products = await response.json();
+			setProducts(products);
+		};
+
+		fetchProducts();
+	}, []);
+
+	return products ? (
+		<div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+			{products.map((product) => (
+				<div key={product.id}>
+					<ProductCard product={product} />
+				</div>
+			))}
+		</div>
+	) : (
+		<div>Loading ...</div>
+	);
 };
 
 export default Home;
